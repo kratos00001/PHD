5183631f7ba8c44e1f909387921940082792f2ac
kratos00001 <yassine.bouhsaien@etu.uae.ac.ma>
2023-12-02 10:18:34 +0100

    azure push

--- BE/src/main/java/ma/uae/CorsConfig.java
+++ BE/src/main/java/ma/uae/CorsConfig.java
@@ -15,8 +15,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
